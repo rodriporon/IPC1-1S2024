12657480395ba7c84d17c516d66852766ccfe914
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2024-03-21 16:26:50 -0600

    agregar ejemplo inicial proyecto 2

---
+++ Proyecto2/frontend/src/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <main>
+      <header>
+        <h1>USocial</h1>
+      </header>
+    </main>
+  );
+}
---
+++ Proyecto2/frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Home";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/foro",
+    element: <h1>Página para mostrar los foros</h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
